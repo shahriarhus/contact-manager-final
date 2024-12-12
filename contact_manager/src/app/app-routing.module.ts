dceba3bb2a0a6a0e92bb0b3668051f25a6744e70
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 15:21:15 +0530

    Updates routes and app component html

--- contact_manager/src/app/app-routing.module.ts
+++ contact_manager/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'add', component: AddContactComponent },
+  { path: 'edit/:id', component: EditContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
