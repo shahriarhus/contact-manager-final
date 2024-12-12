4de6ac054c2b7f259cbdbb9f058b85ddbfa26f0e
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 15:28:38 +0530

    Update app.module.ts

--- contact_manager/src/app/app.module.ts
+++ contact_manager/src/app/app.module.ts
@@ -8,16 +8,41 @@ import { AddContactComponent } from './add-contact/add-contact.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+// Import Angular Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+// Import Angular Forms Module for ngModel binding
+import { FormsModule } from '@angular/forms';
+
+// Import Angular HTTP Client Module
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     AddContactComponent,
     EditContactComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    FormsModule,
+    HttpClientModule,
+    
   ],
   providers: [
     provideAnimationsAsync()
