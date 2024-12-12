927d1c84cf8539d05fe17efb5cc1c29cc500f4a9
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 15:32:12 +0530

    add models and update dashboard ts

--- contact_manager/src/app/dashboard/dashboard.component.ts
+++ contact_manager/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  contacts = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadContacts();
+  }
+
+  loadContacts() {
+    this.http.get('http://localhost:3000/contacts').subscribe((data: any) => {
+      this.contacts = data;
+    });
+  }
+
+  editContact(id: string) {
+    this.router.navigate(['/edit', id]);
+  }
+
+  deleteContact(id: string) {
+    this.http.delete(`http://localhost:3000/contacts/${id}`).subscribe(() => {
+      this.loadContacts();
+    });
+  }
+
+  addContact() {
+    this.router.navigate(['/add']);
+  }
 }
---
+++ contact_manager/src/app/models/contact.model.ts
@@ -0,0 +1,7 @@
+export interface Contact {
+    _id: string;       // Assuming _id is a string
+    name: string;
+    email: string;
+    number: number;    // Defining 'number' as a number type
+  }
+  
