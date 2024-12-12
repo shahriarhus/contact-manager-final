6b51ff4c44f0534d7c5da1ef52e461d752f710de
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 15:08:31 +0530

    Add add_contact component
    ng generate component add_contact --skip-tests

--- contact_manager/src/app/app.module.ts
+++ contact_manager/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
